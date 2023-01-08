c2dbe5ff0b371f56aaad1e51368a88600c2f6da9
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-01-08 20:19:41 +0600

    Footer complete

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 import Navigation from '../Navigation/Navigation';
 
 const Main = () => {
@@ -7,7 +8,7 @@ const Main = () => {
         <div>
             <Navigation />
             <Outlet />
-            <div>Navbar</div>
+            <Footer />
         </div>
     );
 };
