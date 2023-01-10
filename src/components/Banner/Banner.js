7fbffdff1e00b5306fe867a93f96e3e0741faf02
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-01-10 20:41:10 +0600

    Banner completed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import img1 from '../../assets/images/banner/1.jpg';
 
 const Banner = () => {
     return (
-        <div>
-            Banner
+        <div className="hero px-0 lg:px-20 mb-10 lg:mb-28">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src={img1} alt='Car doctor' className="w-full lg:w-1/2 rounded-md" />
+                <div className='text-left ml-0 lg:ml-20'>
+                    <h1 className="text-5xl font-bold">Affordable<br />Price For Car<br />Servicing</h1>
+                    <p className="py-6">There are many variations of passages of  available, but<br />the majority have suffered alteration in some form</p>
+                    <button className="btn btn-primary">Discover More</button>
+                    <button className="btn btn-outline btn-primary ml-5">Latest projects</button>
+                </div>
+            </div>
         </div>
     );
 };
