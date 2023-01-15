6fabd0142c54eea1241c89911dea799051dab81c
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-01-15 21:28:55 +0600

    Products section set-up completed

---
+++ src/components/Products/Products.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <div className='text-center'>
+                <p className='text-primary text-xl font-semibold'>Popular Products</p>
+                <h1 className='text-4xl font-bold my-4'>Browse Our Products</h1>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised<br />words which don't look even slightly believable.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
