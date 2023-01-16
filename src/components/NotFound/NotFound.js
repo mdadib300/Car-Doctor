c92717ddfd2aed753b1d77005d0e096017385636
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-01-16 09:56:03 +0600

    404 route completed

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import image from '../../assets/images/notFound/notFound.gif';
 
 const NotFound = () => {
     return (
         <div>
-            NotFound
+            <h2 className='text-primary text-2xl font-bold text-center mt-10 md:mt-0'>404 | Page not found</h2>
+            <img src={image} alt='page not available' className='mx-auto'></img>
         </div>
     );
 };
